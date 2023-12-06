0c5f472fe49d5f94fe227b6c8bf054fffbbe5d04
CAN GEL <can.gel@hotmail.com>
2023-12-06 13:12:22 +0300

    Absolute path added.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Container } from 'container/container'
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <Container />
   )
 }
 
