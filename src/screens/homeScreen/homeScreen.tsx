ff48a307ba6c0ae235bc7cb5d6bb46830c6b5c1b
CAN GEL <can.gel@hotmail.com>
2023-12-11 12:48:17 +0300

    TabView added.

--- src/screens/homeScreen/homeScreen.tsx
+++ src/screens/homeScreen/homeScreen.tsx
@@ -1,12 +1,57 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View } from 'react-native'
+import React, { useState } from 'react'
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import style from './style'
 import { CustomHeader } from 'components'
+import { Container } from 'container/container'
+import colors from 'assets/colors/colors'
 
 export const HomeScreen = () => {
+
+    const renderIcon = ({ route, focused }: any) => {
+        if (route.key === 'first') {
+            return (
+                <Icon
+                    name="account-group"
+                    size={20}
+                    color={focused ? colors.white : "#88b6ac"}
+                />
+            )
+        }
+    }
+
+    const [index, setIndex] = useState(1)
+    const [routes] = React.useState([
+        { key: 'first' },
+        { key: 'second', title: 'Sohbetler' },
+        { key: 'third', title: 'Güncellemeler' },
+        { key: 'fourth', title: 'Aramalar' }
+    ])
+
     return (
         <View style={style.container}>
             <CustomHeader />
+            <TabView
+                renderTabBar={props => (
+                    <TabBar
+                        {...props}
+                        renderIcon={renderIcon}
+                        style={style.tabBar}
+                        indicatorStyle={style.indicatorStyle}
+                        labelStyle={style.labelStyle}
+                        tabStyle={style.tabStyle}
+                    />
+                )}
+                navigationState={{ index, routes }}
+                onIndexChange={setIndex}
+                renderScene={SceneMap({
+                    first: Container,
+                    second: Container,
+                    third: Container,
+                    fourth: Container
+                })}
+            />
         </View>
     )
 }
--- src/screens/homeScreen/style.ts
+++ src/screens/homeScreen/style.ts
@@ -5,5 +5,19 @@ export default StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: colors.white
+    },
+    tabBar: {
+        backgroundColor: colors.whatsAppGreen
+    },
+    indicatorStyle: {
+        backgroundColor: colors.white,
+        height: 2
+    },
+    labelStyle: {
+        fontSize: 16,
+        textTransform: 'none'
+    },
+    tabStyle: {
+        width: 'auto'
     }
 })
