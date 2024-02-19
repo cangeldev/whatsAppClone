75278adc9b4c979854e611d46c0dcda2a8150881
CAN GEL <can.gel@hotmail.com>
2024-02-19 14:12:42 +0300

    Login screen edited.

--- src/screens/loginScreen/loginScreen.tsx
+++ src/screens/loginScreen/loginScreen.tsx
@@ -1,10 +1,62 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, Text, TextInput, TouchableOpacity, StatusBar } from 'react-native'
+import style from './style'
+import Icon from 'react-native-vector-icons/Entypo' //Icons
+import colors from 'assets/colors/colors'
 
 export const LoginScreen = () => {
     return (
-        <View>
-            <Text>loginScreen</Text>
+        <View style={style.container}>
+            <StatusBar
+                barStyle={"dark-content"}
+                backgroundColor={"white"}
+            />
+            <Icon
+                name={"dots-three-vertical"}
+                size={18}
+                style={style.icon}
+            />
+            <Text style={style.headerTitle}>
+                Telefon numaranızı girin
+            </Text>
+            <Text style={style.headerText}>
+                WhatsApp'ın hesabınızı doğrulaması gerekecek.
+            </Text>
+            <Text style={style.headerInfo}>
+                Numaram nedir?
+            </Text>
+            <View style={style.selectCountryView}>
+                <Text style={style.selectCountry}>
+                    Türkiye
+                </Text>
+                <Icon
+                    name={"triangle-down"}
+                    size={18}
+                    style={style.downIcon}
+                />
+            </View>
+            <View style={style.numberView}>
+                <Text style={style.numberText}>
+                    +    <Text style={style.innerText}>
+                        90
+                    </Text>
+                </Text>
+                <TextInput
+                    placeholder='telefon numarası'
+                    placeholderTextColor={colors.grey}
+                    style={style.numberInput}
+                    autoFocus
+                    keyboardType='number-pad'
+                />
+            </View>
+            <Text style={style.info}>
+                Operatörünüz tarafından ücret uygulanabilir
+            </Text>
+            <TouchableOpacity>
+                <Text style={style.loginButton}>
+                    İleri
+                </Text>
+            </TouchableOpacity>
         </View>
     )
 }
--- src/screens/loginScreen/style.ts
+++ src/screens/loginScreen/style.ts
@@ -1,9 +1,83 @@
-import colors from "assets/colors/colors";
-import { StyleSheet } from "react-native";
+import colors from "assets/colors/colors"
+import { StyleSheet } from "react-native"
 
 export default StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: colors.white
+        backgroundColor: colors.white,
+        alignItems: "center",
+        paddingVertical: 15
     },
+    icon: {
+        color: colors.grey,
+        position: "absolute",
+        right: 15,
+        top: 20
+    },
+    headerTitle: {
+        color: colors.black,
+        fontWeight: "700",
+        fontSize: 18
+    },
+    headerText: {
+        color: colors.black,
+        marginTop: 25
+    },
+    headerInfo: {
+        color: colors.whatsAppGreen,
+    },
+    selectCountryView: {
+        borderBottomWidth: 0.9,
+        borderBottomColor: colors.whatsAppGreen,
+        width: "65%",
+        marginVertical: 15,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    selectCountry: {
+        color: colors.black,
+        fontSize: 17
+    },
+    downIcon: {
+        position: "absolute",
+        right: 0,
+        color: colors.whatsAppGreen
+    },
+    numberView: {
+        flexDirection: "row",
+        width: "65%"
+    },
+    numberText: {
+        borderBottomWidth: 0.9,
+        borderBottomColor: colors.whatsAppGreen,
+        marginRight: 10,
+        paddingHorizontal: 3,
+        textAlignVertical: "center",
+        paddingRight: 10,
+        fontSize: 15
+    },
+    innerText: {
+        color: colors.black,
+        fontSize: 16
+    },
+    numberInput: {
+        fontSize: 16,
+        borderBottomWidth: 1.6,
+        borderBottomColor: colors.whatsAppGreen,
+        flex: 1,
+        paddingVertical: -5
+    },
+    info: {
+        marginTop: 15,
+        flex: 1
+    },
+    loginButton: {
+        color: colors.white,
+        backgroundColor: colors.whatsAppGreen,
+        padding: 10,
+        borderRadius: 25,
+        fontWeight: "600",
+        paddingHorizontal: 25,
+    }
 })
